45b7bc7e43e38043bd0a714e86bcde1314579f5f
Jessica Hong <jessicahong.24@gmail.com>
2019-05-05 01:12:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -51,7 +51,7 @@ end
 end
 
     def shortened_tweet_truncator (tweets)
-      short_tweet = word_substituter(tweets)
+      short_tweet.split = word_substituter(tweets)
       if short_tweet.length > 140 
      puts short_tweet[0..139]+ "..."
     end
